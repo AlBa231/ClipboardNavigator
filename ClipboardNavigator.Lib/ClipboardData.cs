a43575b00b2a85ab7000b7d3f71e66a5b6eaf75e
Oleksandr Barabanov <Oleksandr_Barabanov@epam.com>
2022-08-24 09:58:43 +0300

    feat: added Equals for ClipboardData

--- ClipboardNavigator.Lib/ClipboardData.cs
+++ ClipboardNavigator.Lib/ClipboardData.cs
@@ -2,5 +2,25 @@
 
 public class ClipboardData
 {
-    public string Text { get; set; }
+    public string Text { get; }
+
+    public ClipboardData(string text)
+    {
+        Text = text;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (obj is ClipboardData second)
+            return string.Equals(Text, second.Text);
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return string.GetHashCode(Text);
+    }
 }
---
+++ ClipboardNavigator.Tests/ClipboardDataTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ClipboardNavigator.Tests;
+
+public class ClipboardDataTests
+{
+    [Fact]
+    public void TestClipboardDataEquals()
+    {
+        var data1 = new ClipboardData("test value");
+        var data2 = new ClipboardData("test value");
+
+        var equality = Equals(data1, data2);
+
+        Assert.True(equality);
+    }
+
+    [Fact]
+    public void TestClipboardDataNotEquals()
+    {
+        var data1 = new ClipboardData("test value");
+        var data2 = new ClipboardData("test value 2");
+
+        var equality = Equals(data1, data2);
+
+        Assert.False(equality);
+    }
+}
